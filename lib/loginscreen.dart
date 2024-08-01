f152dec116183b215d5cf0e08cf303154a6fd71c
Nitesh Kumar Sharma <Niteshkumarsharma831@gmai.com>
2024-08-01 15:53:11 +0530

    SimpleLoginPage

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyLoginScreen extends StatefulWidget {
   const MyLoginScreen({super.key});
@@ -13,7 +14,7 @@ class _MyLoginScreenState extends State<MyLoginScreen> {
   bool islogged = false;
 
   final _nameController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final _emailController = TextEditingController();
   final _formkey = GlobalKey<FormState>();
 
   @override
@@ -38,6 +39,50 @@ class _MyLoginScreenState extends State<MyLoginScreen> {
   }
 
   Widget _buildLogin() {
-    return Form(child: Column());
+    return Form(
+      key: _formkey,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: "Username"),
+              validator: (Text) => Text!.isEmpty ? "Enter Name" : null,
+            ),
+            TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(labelText: "Email Address"),
+                validator: (text) {
+                  if (text!.isEmpty) {
+                    return "Email cannot be Empity";
+                  }
+                  final regex = RegExp(r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+                  if (!regex.hasMatch(text)) {
+                    return "Invalid Email";
+                  }
+                  return null;
+                }),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: validate, child: Text("Login"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void validate() {
+    final Form = _formkey.currentState;
+    if (Form?.validate() == false) {
+      return;
+    }
+    setState(() {
+      islogged = true;
+      name = _nameController.text;
+      password = _emailController.text;
+    });
   }
 }
